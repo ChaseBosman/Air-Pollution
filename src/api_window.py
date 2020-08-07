186635043e6d94d0f53c4b14086e29df41d411c7
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-08-07 16:06:19 -0700

    Modified api_window.py and main_window_select.py
    Both classes now work with eachother to open the api menu. Api menu is mostly configured however still need to import data into labels.

--- src/api_window.py
+++ src/api_window.py
@@ -1,41 +1,66 @@
 from tkinter import *
 
 
-class ApiWindow:
-    def __init__(self, master=None):
-        # initialize and set Tk windows
-        self.master = master.geometry("450x300")
-        self.frame = Frame(self.master)
+class ApiWindow(Toplevel):
+    def __init__(self):
+        self.top = Toplevel.__init__(self)
+        self.geometry("450x320")
 
         # call method to create widgets
         self.create_widgets()
 
     def create_widgets(self):
-        self.header_label = Label(self.master, text="Current air quality statistics in your local area")
-        self.loc_label = Label(self.master, text="Data source location:")
-        self.co_label = Label(self.master, text="CO:")
-        self.h_label = Label(self.master, text="Humidity:")
-        self.no2_label = Label(self.master, text="NO2:")
-        self.o3_label = Label(self.master, text="O3:")
-        self.p_label = Label(self.master, text="Pressure:")
-        self.pm10_label = Label(self.master, text="PM 10:")
-        self.pm25_label = Label(self.master, text="PM 2.5:")
-        self.so2_label = Label(self.master, text="SO2:")
-        self.t_label = Label(self.master, text="Temperature:")
-        self.wind_label = Label(self.master, text="Wind speed:")
-
+        self.header_label = Label(self, text="Current air quality statistics in your desired area")
         self.header_label.grid(row=0, column=1)
-        self.loc_label.grid(row=1, column=0)
-        self.h_label.grid(row=2, column=0)
-        self.no2_label.grid(row=3, column=0)
-        self.o3_label.grid(row=4, column=0)
-        self.p_label.grid(row=5, column=0)
-        self.pm10_label.grid(row=6, column=0)
-        self.pm25_label.grid(row=7, column=0)
-        self.so2_label.grid(row=8, column=0)
-        self.t_label.grid(row=9, column=0)
-        self.wind_label.grid(row=10, column=0)
-        self.co_label.grid(row=11, column=0)
+
+        self.loc_label = Label(self, text="Data source location:")
+        self.co_label = Label(self, text="CO:")
+        self.h_label = Label(self, text="Humidity:")
+        self.no2_label = Label(self, text="NO2:")
+        self.o3_label = Label(self, text="O3:")
+        self.p_label = Label(self, text="Pressure:")
+        self.pm10_label = Label(self, text="PM 10:")
+        self.pm25_label = Label(self, text="PM 2.5:")
+        self.so2_label = Label(self, text="SO2:")
+        self.t_label = Label(self, text="Temperature:")
+        self.wind_label = Label(self, text="Wind speed:")
+
+        self.loc_label.grid(row=1, column=0, sticky=E)
+        self.h_label.grid(row=2, column=0, sticky=E)
+        self.no2_label.grid(row=3, column=0, sticky=E)
+        self.o3_label.grid(row=4, column=0, sticky=E)
+        self.p_label.grid(row=5, column=0, sticky=E)
+        self.pm10_label.grid(row=6, column=0, sticky=E)
+        self.pm25_label.grid(row=7, column=0, sticky=E)
+        self.so2_label.grid(row=8, column=0, sticky=E)
+        self.t_label.grid(row=9, column=0, sticky=E)
+        self.wind_label.grid(row=10, column=0, sticky=E)
+        self.co_label.grid(row=11, column=0, sticky=E)
+
+        self.loc_display = Label(self, text=" :")
+        self.co_display = Label(self, text=":")
+        self.h_display = Label(self, text=":")
+        self.no2_display = Label(self, text=":")
+        self.o3_display = Label(self, text=":")
+        self.p_display = Label(self, text=":")
+        self.pm10_display = Label(self, text=":")
+        self.pm25_display = Label(self, text=":")
+        self.so2_display = Label(self, text=":")
+        self.t_display = Label(self, text=":")
+        self.wind_display = Label(self, text=":")
+
+        self.loc_display.grid(row=1, column=1)
+        self.h_display.grid(row=2, column=1)
+        self.no2_display.grid(row=3, column=1)
+        self.o3_display.grid(row=4, column=1)
+        self.p_display.grid(row=5, column=1)
+        self.pm10_display.grid(row=6, column=1)
+        self.pm25_display.grid(row=7, column=1)
+        self.so2_display.grid(row=8, column=1)
+        self.t_display.grid(row=9, column=1)
+        self.wind_display.grid(row=10, column=1)
+        self.co_display.grid(row=11, column=1)
+
 
 
 if __name__ == "__main__":
--- src/main_window_select.py
+++ src/main_window_select.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from api_window import ApiWindow
 
 
 class MainWindowSelect:
@@ -13,8 +14,7 @@ class MainWindowSelect:
         self.frame.pack(fill=BOTH, expand=YES)
 
     def open_api_screen(self):
-        """This method will eventually be implemented to open the api menu/screen"""
-        pass
+        self.new_window = ApiWindow()
 
     def open_compute_screen(self):
         """This method will eventually be implemented to open the computation screen"""
@@ -25,9 +25,9 @@ class MainWindowSelect:
         self.tbl_label = Label(self.frame, text="Select a menu option:").pack(side=TOP, expand=YES)
         # Two buttons, one to visit the API page and one for computing carbon footprint
         self.compute_but = Button(self.frame, text="Compute Your Carbon Footprint", command=lambda:
-            self.open_compute_screen()).pack(side=TOP, expand=YES)
+        self.open_compute_screen()).pack(side=TOP, expand=YES)
         self.api_but = Button(self.frame, text="Find Current Pollution Levels", command=lambda:
-            self.open_api_screen()).pack(side=TOP, expand=YES)
+        self.open_api_screen()).pack(side=TOP, expand=YES)
 
 
 if __name__ == "__main__":
