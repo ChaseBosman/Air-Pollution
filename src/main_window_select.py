ecdc8ec8ea6ab32ee2abf7ce661fee58cb737736
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-08-07 12:40:22 -0700

    Update main_window_select.py
    main_window_select.py now adheres to a more proper convention for class design

--- src/main_window_select.py
+++ src/main_window_select.py
@@ -5,8 +5,8 @@ class MainWindowSelect:
 
     def __init__(self, master=None):
         # initialize and set Tk windows
-        master.geometry("300x200")
-        self.main_window = Frame(master)
+        self.master = master.geometry("300x200")
+        self.main_window = Frame(self.master)
 
         # call method to create widgets and pack them
         self.create_widgets()
@@ -22,11 +22,11 @@ class MainWindowSelect:
 
     def create_widgets(self):
         # Single Label
-        self.tbl_label = Label(root_window, text="Select a menu option:").pack(side=TOP, expand=YES)
+        self.tbl_label = Label(self.main_window, text="Select a menu option:").pack(side=TOP, expand=YES)
         # Two buttons, one to visit the API page and one for computing carbon footprint
-        self.compute_but = Button(root_window, text="Compute Your Carbon Footprint", command=lambda:
+        self.compute_but = Button(self.main_window, text="Compute Your Carbon Footprint", command=lambda:
             self.open_compute_screen()).pack(side=TOP, expand=YES)
-        self.api_but = Button(root_window, text="Find Current Pollution Levels", command=lambda:
+        self.api_but = Button(self.main_window, text="Find Current Pollution Levels", command=lambda:
             self.open_api_screen()).pack(side=TOP, expand=YES)
 
 
